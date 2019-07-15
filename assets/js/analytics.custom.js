aa90ebb847ab07c7b2d5b5deadf634117e076b70
Rubén Antón <rubocoptero@gmail.com>
2019-07-15 19:34:52 +0200

    Set InternalTraffic ga custom dimension

--- assets/js/analytics.custom.js
+++ assets/js/analytics.custom.js
@@ -1,3 +1,5 @@
+var internalTrafficCookieName = "InternalTraffic";
+
 if (window.location.search.indexOf("internal") != -1) {
     setInternalTrafficCookie();
 }
@@ -6,6 +8,8 @@ if (isProduction()) {
     window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
     ga('create', 'UA-37428382-2', 'auto');
 
+    // Custom Dimension - InternalTraffic
+    ga('set', 'dimension1', isInternalTraffic());
 
     ga('require', 'eventTracker');
     ga('require', 'outboundLinkTracker');
@@ -37,12 +41,33 @@ function isProduction() {
 }
 
 function setInternalTrafficCookie() {
-    setCookie("InternalTraffic", "true", 365, "rubocoptero.com");
+    setCookie(internalTrafficCookieName, "true", 365, ".rubocoptero.com");
 
     function setCookie(cname, cvalue, exdays, cdomain) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays*24*60*60*1000));
         var expires = "expires="+ d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/ ; domain=" + cdomain;
+    }
+}
+
+function isInternalTraffic() {
+    return !! getCookie(internalTrafficCookieName);
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
     }
 }
